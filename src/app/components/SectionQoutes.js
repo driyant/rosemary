bcb22b85957e459a0f9a25a31e9a9adb162e4788
Alaq A.Ryanta <message.driyant@outlook.com>
2025-05-07 00:16:39 +0700

    feat: init v3

---
+++ src/app/components/SectionQoutes.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Box, Text, Heading } from "@chakra-ui/react";
+import restoDining from "../../../public/images/resto-dining.jpg";
+
+const SectionQuote = () => {
+  return (
+    <>
+      <Box
+        as="section"
+        h="50vh"
+        display={{ base: "flex" }}
+        flexFlow={{ base: "column" }}
+        justifyContent="space-evenly"
+        alignItems="flex-start"
+        bgImage={restoDining}
+        bgSize="cover"
+        bgPos="center"
+        px={{ base: "1rem" }}
+        color="#FFFFFF"
+        position="relative"
+      >
+        <Box
+          bgGradient="linear(135deg, #A46E50, #D7AB72)"
+          opacity="85%"
+          w="100%"
+          h="100%"
+          left="0"
+          position="absolute"
+        ></Box>
+        <Text
+          fontFamily="var(--Libre-Baskerville)"
+          fontSize="1.5rem"
+          zIndex="2"
+          mx={{ lg: "auto" }}
+        >
+          If you are a happy person around food, you can be a professional chef.
+          It is fueled by passion.
+        </Text>
+        <Box zIndex="2" mx={{ lg: "auto" }} textAlign={{ lg: "center" }}>
+          <Heading
+            as="h6"
+            textTransform="uppercase"
+            fontFamily="var(--Montserrat)"
+          >
+            John Doe
+          </Heading>
+          <Text textTransform="uppercase">chef executive</Text>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default SectionQuote;
--- src/app/discover/page.js
+++ src/app/discover/page.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Nav from "../components/Nav";
+// import SectionQuote from "../components/SectionQoutes";
+// import { Box, Heading, Text, Image } from "@chakra-ui/react";
+// import steak from "../../../public/images/steak.jpg";
+// import restaurantChef from "../../../public/images/restaurant_chef.jpg";
+// import SectionTodayMenu from "../components/SectionTodayMenu";
+// import SectionService from "../../components/SectionService/SectionService";
+// import Maps from "../../components/Maps/Maps";
+// import Footer from "../../components/Footer/Footer";
 
-const discover = () => {
-  return <div>discover</div>;
+const Discover = () => {
+  return (
+    <>
+      <Nav />
+    </>
+  );
 };
 
-export default discover;
+export default Discover;
